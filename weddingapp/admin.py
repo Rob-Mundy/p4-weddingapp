d2f8830f69898341e919585593b83753b37b71cb
Rob-Mundy <108663330+Rob-Mundy@users.noreply.github.com>
2023-04-30 09:32:55 +0100

    Update admin.py
    Amend comment to slugfield

--- weddingapp/admin.py
+++ weddingapp/admin.py
@@ -12,6 +12,6 @@ class GuestAdmin(admin.ModelAdmin):
     search_fields = ['guest_name', 'invited']
     prepopulated_fields = {'slug': ('user', 'guest_name',)}
     # slugfield prepopulated with user + guest_name so that 
-    # slug is unique for user but guest_name is not unqique
+    # slug is unique for user as guest_name is not unqique
     # site-wide
     list_filter = ('guest_name', 'attending')
