7af856c81d04e70e2157886b59caaf63119b1b96
Rob-Mundy <108663330+Rob-Mundy@users.noreply.github.com>
2023-04-30 09:31:50 +0100

    Update admin.py
    Add comment to code regarding slugfield prepopulation

--- weddingapp/admin.py
+++ weddingapp/admin.py
@@ -11,4 +11,7 @@ class GuestAdmin(admin.ModelAdmin):
     list_display = ('guest_name', 'slug', 'invited', 'attending', 'plus_one_attending')
     search_fields = ['guest_name', 'invited']
     prepopulated_fields = {'slug': ('user', 'guest_name',)}
+    # slugfield prepopulated with user + guest_name so that 
+    # slug is unique for user but guest_name is not unqique
+    # site-wide
     list_filter = ('guest_name', 'attending')
